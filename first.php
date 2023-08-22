1cbbe6045d84a218ab557b5bc8203d0820bd02c6
Hannah Holmes <hjholmes@hawaii.edu>
2023-08-22 10:37:10 +0200

    running my first php page on the local server

---
+++ first.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xlmns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic PHP Page</title>
+</head>
+<body>
+    <!--Script 1.2 - first.php -->
+    <p>This is standard HTML. I made a change.</p>
+<?php
+?>
+</body>
+</html>
